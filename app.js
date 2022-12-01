f1670fa09b39920dda1ae701cb11d9ae9092f43f
juansrz <95307238+juansrz@users.noreply.github.com>
2022-12-01 19:00:24 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 
 
 const hero = document.querySelector(".hero");
-// const slider = document.querySelector(".slider");
 const headline = document.querySelector(".headline");
 const headline2 = document.querySelector(".headline2");
 
@@ -10,5 +9,5 @@ const tl = new TimelineMax();
 
 tl.fromTo(hero, 1, {height: "0%"}, { height: '80%', ease: Power2.easeInOut})
 .fromTo (hero, 1.2, {width: '70%'}, {width: '100%', ease: Power2.easeInOut })
-// .fromTo (slider, 1.2, {x: "-100%"}, {x: '0%', ease: Power2.easeInOut }, "-=1.6")
+
 
