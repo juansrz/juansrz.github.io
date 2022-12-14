0adda69df3d525af60e0f1ce653add7a6aaf38fc
juansrz <95307238+juansrz@users.noreply.github.com>
2022-12-14 02:13:11 +0100

    update

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Efecto pantalla inicial
 
 
 const hero = document.querySelector(".hero");
@@ -8,6 +9,50 @@ const headline2 = document.querySelector(".headline2");
 const tl = new TimelineMax();
 
 tl.fromTo(hero, 1, {height: "0%"}, { height: '80%', ease: Power2.easeInOut})
-.fromTo (hero, 1.2, {width: '70%'}, {width: '100%', ease: Power2.easeInOut })
+.fromTo (hero, 1.2, {width: '70%'}, {width: '100%', ease: Power2.easeInOut });
 
 
+// Botón scroll to top
+
+const scrollBtn = document.querySelector(".scroll-to-top");
+
+const refreshButtonVisibility = () => {
+
+    if (document.documentElement.scrollTop <= 4000) {
+        scrollBtn.style.display = "none";
+    } else {
+        scrollBtn.style.display = "block";
+    }
+
+};
+
+refreshButtonVisibility();
+    
+scrollBtn.addEventListener('click', () => {
+    document.documentElement.scrollTop = 0;
+    document.body.scrollTop = 0;
+});
+
+document.addEventListener('scroll', (e) => {
+    refreshButtonVisibility();
+});
+
+// Pop-up
+
+function toggle() {
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    var pop_up = document.getElementById('pop_up');
+    pop_up.classList.toggle('active');
+    
+    
+};
+
+function toggle2() {
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    var pop_uptwo = document.getElementById('pop_uptwo');
+    pop_up.classList.toggle('active');
+    
+    
+};
